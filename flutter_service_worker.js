8bb27b0f8535d310ae12fcb81b7776aec9d188e1
Ali Fathi <Alphanad.Official@gmail.com>
2023-06-13 21:25:47 +0330

    Change a text.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -21,9 +21,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "3f1739122166cc86da09285757fdcaf9",
 "icons/Icon-maskable-192.png": "8b5c4f1280b823ead1b0be7da77daf7e",
 "icons/Icon-maskable-512.png": "3f1739122166cc86da09285757fdcaf9",
-"index.html": "3189c58d0b684359d8fd5a13b198ddf2",
-"/": "3189c58d0b684359d8fd5a13b198ddf2",
-"main.dart.js": "bab008e42b5b7cd579faef2ad5517752",
+"index.html": "4a4c123054d3eeda826f90c6412ae383",
+"/": "4a4c123054d3eeda826f90c6412ae383",
+"main.dart.js": "490a640cea818ad7957fc05a679fccfc",
 "manifest.json": "cf96ffba9421a8fb52a5f99d0a661523",
 "version.json": "6fc0fe2b6e08ebf5329d1f385d9544f5"
 };
